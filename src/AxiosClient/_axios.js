21069e2597c0a4100442f7afcc229d5ada806f2a
B@MacbookPro <68946059+MBN95@users.noreply.github.com>
2021-05-22 17:20:55 +0100

    changed base url to match heroku api.. this allows us to receive data from api thats stored on cloud.... firebase hosting was setup and this project upon a new build will also create a fresh build and deploy on firebase-site.

--- src/AxiosClient/_axios.js
+++ src/AxiosClient/_axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const herokuBaseURL = "https://tinder-backend-clone-0.herokuapp.com/";
+const localURL = "http://localhost:8001/";
 const instance = axios.create({
-  baseURL: "http://localhost:8001/"
+  baseURL: herokuBaseURL
 });
 
 export default instance;
